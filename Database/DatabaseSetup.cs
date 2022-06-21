fe7284ce95fcc7b084555631403728fd441d96d6
Ryan Henrique de Aguiar dos Santos <Gtsupera12@gmail.com>
2022-06-21 08:59:18 -0300

    refactor:add computer repository save

--- Database/DatabaseSetup.cs
+++ Database/DatabaseSetup.cs
@@ -1,42 +1,33 @@
 using Microsoft.Data.Sqlite;
 
-
 namespace LabManager.Database;
 
 class DatabaseSetup
 {
-   private DatabaseConfig databaseConfig;
+    private readonly DatabaseConfig _databaseConfig;
 
-   public DatabaseSetup(DatabaseConfig databaseConfig)
-   {
-       this.databaseConfig = databaseConfig;
-       CreateTableComputer();
-   }
-   
-    private void CreateTableComputer()
+    public DatabaseSetup(DatabaseConfig databaseConfig)
     {
-        var connection = new SqliteConnection(databaseConfig.ConnectionString);
+        _databaseConfig = databaseConfig;
+        CreateComputerTable();
+    }
+
+    private void CreateComputerTable()
+    {
+        
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
         connection.Open();
 
         var command = connection.CreateCommand();
         command.CommandText = @"
-        CREATE TABLE IF NOT EXISTS Computers(
-            id int not null primary key,
-            ram varchat(100) not null,
-            processor varchar(100) not null
-            
-        );
-
+            CREATE TABLE IF NOT EXISTS Computers(
+                id int not null primary key,
+                ram varchar(100) not null,
+                processor varchar(100) not null
+            );
         ";
 
         command.ExecuteNonQuery();
         connection.Close();
     }
-    
-    public void CreateTableLab() 
-    {
-        
-
-    }
-
 }
